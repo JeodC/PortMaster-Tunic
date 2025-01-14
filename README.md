ea36a3d0363f8007070a6610fe462db03ee0e651
Jeod <47716344+JeodC@users.noreply.github.com>
2025-01-13 20:12:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PortMaster - Tunic
+This port runs on Retroid Pocket 5 with wine64 and box64. It has been tested with RockNIX. To install, copy the port directory `tunic` and `Tunic.sh` file to your `ports` folder, and then copy your `Tunic` game data into `tunic/data`.
+
+You need `wine-mono` and `dxvk` dependencies. The launchscript installs dxvk for you if it doesn't exist already, and you will be prompted if you need `wine-mono`.
---
+++ Tunic.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR=/$directory/ports/tunic
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Display loading splash
+[ "$CFW_NAME" == "muOS" ] && $ESUDO $GAMEDIR/splash "splash.png" 1
+$ESUDO $GAMEDIR/splash "splash.png" 30000 & 
+
+# Exports
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export WINEPREFIX=~/.wine64
+export WINEDEBUG=-all
+
+# Install dependencies
+if ! winetricks list-installed | grep -q "^dxvk$"; then
+    pm_message "Installing dependencies."
+    winetricks dxvk
+fi
+
+# Config Setup
+mkdir -p $GAMEDIR/config
+bind_directories "$WINEPREFIX/drive_c/users/root/AppData/LocalLow/Andrew Shouldice/Secret Legend" "$GAMEDIR/config"
+
+# Run the game
+$GPTOKEYB "Tunic.exe" -c "./tunic.gptk" &
+box64 wine64 "./data/Tunic.exe"
+
+# Kill processes
+wineserver -k
+pm_finish
