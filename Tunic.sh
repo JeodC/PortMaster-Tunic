2da9110c2827e5c2352815543ea6c8d2838583d2
Jeod <47716344+JeodC@users.noreply.github.com>
2025-03-17 16:45:13 -0400

    Update Tunic.sh

--- Tunic.sh
+++ Tunic.sh
@@ -14,25 +14,46 @@ get_controls
 
 # Variables
 GAMEDIR="/$directory/windows/tunic"
+EXEC="Tunic.exe"
 
-# CD and set permissions
+# CD and set log
 cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-$ESUDO chmod +x -R $GAMEDIR/*
 
 # Display loading splash
-[ "$CFW_NAME" == "muOS" ] && $ESUDO $GAMEDIR/splash "splash.png" 1
-$ESUDO $GAMEDIR/splash "splash.png" 30000 & 
+$ESUDO $GAMEDIR/splash "$GAMEDIR/splash.png" 30000 & 
 
 # Exports
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export WINEPREFIX=~/.wine64
 export WINEDEBUG=-all
 
+export BOX64_DYNAREC_SAFEFLAGS=1
+export BOX64_DYNAREC_FASTNAN=1
+export BOX64_DYNAREC_FASTROUND=1
+export BOX64_DYNAREC_X87DOUBLE=0
+export BOX64_DYNAREC_BIGBLOCK=3
+export BOX64_DYNAREC_STRONGMEM=0
+export BOX64_DYNAREC_FORWARD=512
+export BOX64_DYNAREC_CALLRET=1
+export BOX64_DYNAREC_WAIT=1
+export BOX64_AVX=0
+export BOX64_MAXCPU=8
+export BOX64_UNITYPLAYER=1
+
+# Determine architecture
+if file "$GAMEDIR/data/$EXEC" | grep -q "PE32" && ! file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEARCH=win32
+    export WINEPREFIX=~/.wine32
+elif file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEPREFIX=~/.wine64
+else
+    echo "Unknown file format"
+fi
+
 # Install dependencies
-if ! winetricks list-installed | grep -q "^dxvk$"; then
+if ! winetricks list-installed | grep -q "^dxvk2041$"; then
     pm_message "Installing dependencies."
-    winetricks dxvk
+    winetricks dxvk2041
 fi
 
 # Config Setup
@@ -40,8 +61,8 @@ mkdir -p $GAMEDIR/config
 bind_directories "$WINEPREFIX/drive_c/users/root/AppData/LocalLow/Andrew Shouldice/Secret Legend" "$GAMEDIR/config"
 
 # Run the game
-$GPTOKEYB "Tunic.exe" -c "./tunic.gptk" &
-box64 wine64 "./data/Tunic.exe"
+$GPTOKEYB "$EXEC" -c "$GAMEDIR/tunic.gptk" &
+box64 wine "$GAMEDIR/data/$EXEC"
 
 # Kill processes
 wineserver -k
